3e616bf6010c23d37e5e4cdcc3aa2810598ac76f
eSilence <eSilence@yandex.ru>
2016-09-17 17:50:27 +0300

    + pom.xml

---
+++ HW_2/src/main/java/main/Main.java
@@ -0,0 +1,62 @@
+package main;
+
+import accounts.AccountService;
+import accounts.UserProfile;
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.SignInServlet;
+import servlets.SignUpServlet;
+
+/**
+ * ﻿﻿Задача:
+ Написать сервер с двумя сервлетами:
+ SignUpServlet для обработки запросов на signup и
+ SignInServlet для обработки запросов на signin
+
+ Сервлеты должны слушать POST запросы с параметрами
+ login
+ password
+
+ При получении POST запроса на signup сервлет SignUpServlet должн запомнить логин и пароль в AccountService.
+ После этого польователь с таким логином считается зарегистрированным.
+ При получении POST запроса на signin, после регистрации, SignInServlet проверяет,
+ логин/пароль пользователя. Если пользователь уже зарегистрирован, север отвечает
+
+ Status code (200)
+ и текст страницы:
+ Authorized: login
+
+ если нет:
+ Status code (401)
+ текст страницы:
+ Unauthorized
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        AccountService accountService = new AccountService();
+
+        accountService.addNewUser(new UserProfile("admin"));
+        accountService.addNewUser(new UserProfile("test"));
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(new SignUpServlet(accountService)), "/signup");
+        context.addServlet(new ServletHolder(new SignInServlet(accountService)), "/signin");
+
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setResourceBase("public_html");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{resource_handler, context});
+
+        Server server = new Server(8080);
+        server.setHandler(handlers);
+
+        server.start();
+        server.join();
+    }
+
+}
---
+++ HW_2/src/main/java/servlets/SignUpServlet.java
@@ -0,0 +1,68 @@
+package servlets;
+
+import accounts.AccountService;
+import accounts.UserProfile;
+import com.google.gson.Gson;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SignUpServlet extends HttpServlet {
+    @SuppressWarnings({"FieldCanBeLocal", "UnusedDeclaration"}) //todo: remove after module 2 home work
+    private final AccountService accountService;
+
+    public SignUpServlet(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    //get public user profile
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException {
+        //todo: module 2 home work
+    }
+
+    //sign up
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException {
+        String login = request.getParameter("login");
+        String pass = request.getParameter("password");
+
+        if (login == null || pass == null) {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        UserProfile profile = accountService.getUserByLogin(login);
+        if (profile != null) {
+            response.setContentType("text/html;charset=utf-8");
+            response.getWriter().println("This login already exists");
+            response.setStatus(HttpServletResponse.SC_CONFLICT);
+            return;
+        }
+
+        accountService.addNewUser(new UserProfile(login, pass));
+        Gson gson = new Gson();
+        String json = gson.toJson(accountService.getUserByLogin(login));
+        response.setContentType("application/json; charset=utf-8");
+        response.getWriter().println(json);
+        response.getWriter().println("Success registered");
+        response.setStatus(HttpServletResponse.SC_OK);
+
+    }
+
+    //change profile
+    public void doPut(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException {
+        //todo: module 2 home work
+    }
+
+    //unregister
+    public void doDelete(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException, IOException {
+        //todo: module 2 home work
+    }
+}
